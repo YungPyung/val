6486425a752464702fe10ea779979e1adf9a3391
YungPyung <samuel.p.kim01@gmail.com>
2025-07-28 16:03:07 -0500

    minimalist plugin

---
+++ plugins/minimalist.py
@@ -0,0 +1,28 @@
+from phy import IPlugin, connect
+
+"""Prevent unwanted views from instantiating
+
+minimal_list: list
+
+- only edit the minimal_list
+- use --debug to check accurate listing
+
+"""
+
+class Minimalist(IPlugin):
+    def attach_to_controller(self, controller):
+        
+        default_list = list(controller.view_creator)
+        print(f"Default views: {default_list}")
+        
+        minimal_list = ['ClusterView', 'WaveformView', 'AmplitudeView', 'CorrelogramView', 'FeatureView'] 
+        
+        controller.view_creator = {
+            view: method 
+            for view, method in controller.view_creator.items()
+            if view in minimal_list
+        }
+
+        print(f"Filtered views: {list(controller.view_creator)}")
+
+    
